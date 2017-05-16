b6a2191f809ed6626381ec29799c890124541f45
Andrii P <pelekhav@gmail.com>
2017-05-15 21:27:54 -0600

    Creating project

---
+++ src/main/java/com/andriiP/carsSaver/CarsSaverApplication.java
@@ -0,0 +1,12 @@
+package com.andriiP.carsSaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsSaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsSaverApplication.class, args);
+	}
+}
