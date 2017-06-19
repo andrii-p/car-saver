26c71ecb355ccbcd44dc9eaa6750b42043c00507
Andrii P <pelekhav@gmail.com>
2017-06-18 22:37:55 -0600

    Adding Jsoup

--- src/main/java/com/andriiP/carsSaver/CarsSaverApplication.java
+++ src/main/java/com/andriiP/carsSaver/CarsSaverApplication.java
@@ -4,10 +4,14 @@ import com.rometools.rome.feed.synd.SyndEntry;
 import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.io.SyndFeedInput;
 import com.rometools.rome.io.XmlReader;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
@@ -15,6 +19,8 @@ public class CarsSaverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarsSaverApplication.class, args);
+
+		List<String> links = new ArrayList<>();
 
 		try {
 			URL url = new URL("https://denver.craigslist.org/search/cto?format=rss");
@@ -24,17 +30,21 @@ public class CarsSaverApplication {
 			SyndFeedInput input = new SyndFeedInput();
 			SyndFeed feed = input.build(new XmlReader(url));
 
-			List<SyndEntry> entries = feed.getEntries();
+			feed.getEntries().forEach(entry -> links.add(entry.getLink()));
 
-			for (SyndEntry entry : entries){
+			for (SyndEntry entry : feed.getEntries()){
 				System.out.println(entry.getLink());
 				System.out.println("===============================================================");
 			}
+
+			Document doc = Jsoup.connect(links.get(0)).get();
+			System.out.println(doc);
 
 			//System.out.println(entries);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
 
 	}
 }
